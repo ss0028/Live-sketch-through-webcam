d40d51393770fd2058026ba3b73b81a2ab72ed89
Shikha Singh <31015605+ss0028@users.noreply.github.com>
2018-09-14 01:37:40 +0530

    Update code.py

--- code.py
+++ code.py
@@ -5,11 +5,25 @@ import cv2
 #Creating a function to generate live sketch of the object detected by webcam
 def sketch(img):
     
-    #The 'if' statement is used to check that images are passed correctly
+    #The 'if' statement is used to check that frames are passed correctly
     if img is not None:
         print("image is present")
         print(len(img.shape))
-    return img
+        
+    #converting the image to grayscale color space to apply thresholding
+    img_gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
+    #using blurring operation(Gaussian Blur) to smoothen & clean up any noise in the image
+    img_gray_blur=cv2.GaussianBlur(img_gray,(3,3),0)
+    
+    #for edge detection, using 'Canny' method
+    canny_edges=cv2.Canny(img_gray_blur,10,70)
+    
+    #the threshold operation returns 2 functions:
+    #'ret' is a boolean variable returning True or False if the image is detected or not respectively
+    #'mask' is our final sketch after applying all image manipulation functions
+    ret,mask=cv2.threshold(canny_edges,70,255,cv2.THRESH_TOZERO)
+    return mask
 
 #Creating a cap variable which pulls image from the webcam
 cap=cv2.VideoCapture(1)
@@ -32,5 +46,9 @@ while True:
     if cv2.waitKey(1)==13:
         break
         
+#cap.release() is used to release the camera. If it is not applied, leads to either abnormal termination or 
+#hanging & locking up of the kernel.
 cap.release()
+
+#the below command is used to close all open windows of openCV
 cv2.destroyAllWindows()
